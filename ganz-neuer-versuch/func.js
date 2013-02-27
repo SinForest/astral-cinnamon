61a13959b9aab0892c687cd561dd17b478a757a4
Patrick Dammann <patrick@thesinforest.eu>
2013-02-27 22:51:43 +0100

    am_verzweifeln
    Versuche immernoch, die Funktionen zu exportieren.

---
+++ ganz-neuer-versuch/func.js
@@ -0,0 +1,51 @@
+			function getFromText(input)
+			{
+				var vectors = new Array();
+				jQuery.get(input, function(data) {
+					var tmp_1 = data.split("/n");
+					for(var i=0; i < tmp_1.length(); i++)
+					{
+						vectors[i] = new Array();
+						var tmp_2 = tmp_1[i].split(",");
+						for(var j=0; j < tmp_2.length(); j++)
+						{
+							vectors[i][j] = tmp_2[j].split(" ");
+						}
+					}
+				}
+				return vectors;
+			}
+			
+			function resetCanvas(i_canvas)
+			{
+				var c = document.getElementById(i_canvas);
+				var ctx = c.getContext("2d");
+				ctx.fillStyle = "#1F1F1F"; //Dunkelgrau füllen;
+				ctx.fillRect(0,0,canW,canH); //Füllen vom Canvas;
+			}
+			
+			function drawStreets(i_canvas, i_vectors)
+			{
+				var c = document.getElementById(i_canvas);
+				var ctx = c.getContext("2d");
+				ctx.strokeStyle = "#0040FF"; //Straßen sind blau;
+				var Xmax = rightDownLongitude - leftUpperLongitude;					
+				var Ymax = rightDownLatitude - leftUpperLatitude;
+				for (var stra=0; stra < i_vectors.length; stra++)
+				{
+					for (var vec=0; vec < i_vectors[stra].length; vec++)
+					{
+						i_vectors[stra][vec][0] = (i_vectors[stra][vec][0] - leftUpperLongitude) / (Xmax/canW);
+						i_vectors[stra][vec][1] = (i_vectors[stra][vec][1] - leftUpperLatitude) / (Ymax/canH);
+						if(vec = 0)
+						{
+							ctx.moveTo( Math.round(i_vectors[stra][0][0]) , Math.round(i_vectors[stra][0][1]) );
+						}
+						else
+						{
+							ctx.lineTo( Math.round(i_vectors[stra][vec][0]) , Math.round(i_vectors[stra][vec][1]) );
+						}
+					}
+				}
+				ctx.stroke();
+			}
