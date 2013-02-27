d338dca4d1235031e53801ee5c107a2bc0bbb8ed
Felix <abx@ef.de>
2013-02-27 19:35:11 +0100

    ortung v0.02

---
+++ strassen-test2/crawl.php
@@ -0,0 +1,5 @@
+<?php
+$homepage = file_get_contents('http://129.206.228.72:8280/OSMGeometrieService/osmgeomservice/roads/8.659085/49.434311/8.712819/49.390474');
+//$homepage = file_get_contents('http://google.com/');
+echo $homepage;
+?>
