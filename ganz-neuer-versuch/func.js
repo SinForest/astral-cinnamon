7b64458ba87943bad6be9985c30b62aa95db0834
Felix <abx@ef.de>
2013-02-28 01:17:36 +0100

    Modularisierung v2

--- ganz-neuer-versuch/func.js
+++ ganz-neuer-versuch/func.js
@@ -23,19 +23,20 @@
 				$.ajax({
 					url: input,
 					dataType: "text",
-					complete: function(data){
-					var tmp_1 = data.split("/n");
-					for(var i=0; i < tmp_1.length(); i++)
-					{
-						vectors[i] = new Array();
-						var tmp_2 = tmp_1[i].split(",");
-						for(var j=0; j < tmp_2.length(); j++)
-						{
-							vectors[i][j] = tmp_2[j].split(" ");
-							alert(vectors[i][j][1]);
-						}
+					async: false,
+					success: function(data){
+						var tmp_1 = data.split("\n");
+						for(var i=0; i < tmp_1.length; i++) {
+							vectors[i] = new Array();							
+							var tmp_2 = tmp_1[i].split(",");							
+							for(var j=0; j < tmp_2.length; j++)
+							{
+								vectors[i][j] = tmp_2[j].split(" ");
+								//alert("("+i+","+j+"): "+vectors[i][j][0] + ":" + vectors[i][j][1]);
+							}
+						}						
 					}
-				}});
+				});	
 				return vectors;
 			}
 			
